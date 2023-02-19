108cbb4144c1d8682890ca755701555182e82c56
flxdot <Felixfang@gmx.de>
2023-02-19 11:25:53 +0100

    fix lint issue

--- src/routes/cv/data.ts
+++ src/routes/cv/data.ts
@@ -1,7 +1,7 @@
 import type WorkExperience from './types';
 
 import _experience from '$data/work-experience.json';
-export let experience = _experience as WorkExperience[];
+export const experience = _experience as WorkExperience[];
 
 experience
 	.sort((a, b) => a.start - b.start || (a.end ? a.end : 9999) - (b.end ? b.end : 9999))
